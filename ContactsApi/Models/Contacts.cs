15aed947907d6e0ed724e67ed59fd222ef73ddaf
Simeon Petkov Stoykov <simeon_stoykov@mail.bg>
2017-10-20 00:11:03 +0300

    removed vs folder

--- ContactsApi/Models/Contacts.cs
+++ ContactsApi/Models/Contacts.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContactsApi.Models
 {
     public class Contacts
     {
+        [MinLength(3)]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool IsFamilyMember { get; set; }
